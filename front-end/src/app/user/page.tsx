3fe5015fa7f313d34d0ad6b3daa1cfe52fbc6924
Батмандал Бямба <23LP5716@G301-CC0209.local>
2024-02-08 15:35:30 +0800

    front

--- front-end/src/app/user/page.tsx
+++ front-end/src/app/user/page.tsx
@@ -1,159 +1,39 @@
 import { Stack, Typography } from "@mui/material";
-import CreateIcon from "@mui/icons-material/Create";
+
+import { Action, UserInput } from "@/components/UserInput";
 import {
   ForwardToInboxOutlined,
   HistoryOutlined,
+  LogoutOutlined,
   PersonOutlined,
   PhoneOutlined,
 } from "@mui/icons-material";
 
+const userInformation = [
+  { label: "Таны нэр", icon: <PersonOutlined />, text: "УгтахБаяр" },
+  { label: "Утасны дугаар", icon: <PhoneOutlined />, text: "88883345" },
+  {
+    label: "Имэйл хаяг",
+    icon: <ForwardToInboxOutlined />,
+    text: "Ugtakhbayr@gmail.com",
+  },
+];
+
 export default function User() {
   return (
-    <Stack>
-      <Stack maxWidth="432px">
-        <Stack>
-          <Stack></Stack>
-          <Typography></Typography>
-        </Stack>
-        <Stack>
-          <Stack
-            width="100%"
-            py={1}
-            px="20px"
-            bgcolor="#F6F6F6"
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Stack direction="row" gap={1}>
-              <Stack
-                borderRadius="50%"
-                bgcolor="white"
-                padding="12px"
-                width="fit-content"
-              >
-                <PersonOutlined />
-              </Stack>
-              <Stack justifyContent="center">
-                <Typography fontSize="12px" fontWeight="400" color="#888A99">
-                  Таны нэр
-                </Typography>
-                <Typography fontSize="16px" fontWeight="400">
-                  T-Rex
-                </Typography>
-              </Stack>
-            </Stack>
-            <CreateIcon />
-          </Stack>
-          <Stack
-            width="100%"
-            py={1}
-            px="20px"
-            bgcolor="#F6F6F6"
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Stack direction="row" gap={1}>
-              <Stack
-                borderRadius="50%"
-                bgcolor="white"
-                padding="12px"
-                width="fit-content"
-              >
-                <PhoneOutlined />
-              </Stack>
-              <Stack justifyContent="center">
-                <Typography fontSize="12px" fontWeight="400" color="#888A99">
-                  Утасны дугаар
-                </Typography>
-                <Typography fontSize="16px" fontWeight="400">
-                  99999999
-                </Typography>
-              </Stack>
-            </Stack>
-            <CreateIcon />
-          </Stack>
-          <Stack
-            width="100%"
-            py={1}
-            px="20px"
-            bgcolor="#F6F6F6"
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Stack direction="row" gap={1}>
-              <Stack
-                borderRadius="50%"
-                bgcolor="white"
-                padding="12px"
-                width="fit-content"
-              >
-                <ForwardToInboxOutlined />
-              </Stack>
-              <Stack justifyContent="center">
-                <Typography fontSize="12px" fontWeight="400" color="#888A99">
-                  Имэйл хаяг
-                </Typography>
-                <Typography fontSize="16px" fontWeight="400">
-                  Rexy@mail.com
-                </Typography>
-              </Stack>
-            </Stack>
-            <CreateIcon />
-          </Stack>
-          <Stack
-            width="100%"
-            py={1}
-            px="20px"
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Stack direction="row" gap={1}>
-              <Stack
-                borderRadius="50%"
-                bgcolor="white"
-                padding="12px"
-                width="fit-content"
-                border="1px solid #F6F6F6"
-              >
-                <HistoryOutlined />
-              </Stack>
-              <Stack justifyContent="center">
-                <Typography fontSize="16px" fontWeight="400">
-                  Захиалгын түүх
-                </Typography>
-              </Stack>
-            </Stack>
-          </Stack>
-          <Stack
-            width="100%"
-            py={1}
-            px="20px"
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Stack direction="row" gap={1}>
-              <Stack
-                borderRadius="50%"
-                bgcolor="white"
-                padding="12px"
-                width="fit-content"
-                border="1px solid #F6F6F6"
-              >
-                <HistoryOutlined />
-              </Stack>
-              <Stack justifyContent="center">
-                <Typography fontSize="16px" fontWeight="400">
-                  Захиалгын түүх
-                </Typography>
-              </Stack>
-            </Stack>
-          </Stack>
-        </Stack>
+    <Stack gap={5} alignItems="center">
+      <Stack></Stack>
+      <Typography fontWeight={700} fontSize="28px">
+        УгтахБаяр
+      </Typography>
+      <Stack gap={2}>
+        {userInformation.map((item) => {
+          return (
+            <UserInput label={item.label} icon={item.icon} text={item.text} />
+          );
+        })}
+        <Action title="Захиалгын түүх" icon2={<HistoryOutlined />} />
+        <Action title="Гарах" icon2={<LogoutOutlined />} />
       </Stack>
     </Stack>
   );
---
+++ front-end/src/components/UserInput.tsx
@@ -0,0 +1,80 @@
+import { CreateOutlined, PersonOutlined } from "@mui/icons-material";
+import { Stack, TextField, Typography } from "@mui/material";
+import { ReactNode } from "react";
+
+type UserType = {
+  label: string;
+  icon: ReactNode;
+  text: string;
+};
+
+export function UserInput(props: UserType) {
+  const { label, icon, text } = props;
+  return (
+    <Stack
+      width="392px"
+      bgcolor="#F6F6F6"
+      direction="row"
+      justifyContent="space-between"
+      py={1}
+      px="20px"
+      alignItems="center"
+    >
+      <Stack direction="row" alignItems="center" gap={1}>
+        <Stack
+          width="48px"
+          height="48px"
+          borderRadius="50%"
+          border="solid 1px #EEEFF2"
+          display="grid"
+          sx={{ placeContent: "center", background: "white" }}
+        >
+          {icon}
+        </Stack>
+        <Stack>
+          <Typography fontWeight="400" fontSize="12px" color="#888A99">
+            {label}
+          </Typography>
+          <Typography fontWeight={400} fontSize="16px">
+            {text}
+          </Typography>
+        </Stack>
+      </Stack>
+      <Stack color="#18BA51">
+        <CreateOutlined />
+      </Stack>
+    </Stack>
+  );
+}
+
+type PropsAction = {
+  title: string;
+  icon2: ReactNode;
+};
+export function Action(props: PropsAction) {
+  const { title, icon2 } = props;
+  return (
+    <Stack
+      direction="row"
+      width="392px"
+      py={1}
+      px="20px"
+      alignItems="center"
+      gap={1}
+    >
+      <Stack
+        width="48px"
+        height="48px"
+        borderRadius="50%"
+        border="solid 1px #EEEFF2"
+        display="grid"
+        sx={{ placeContent: "center", background: "white" }}
+      >
+        {icon2}
+      </Stack>
+      <Typography fontWeight={400} fontSize="16px">
+        {title}
+      </Typography>
+    </Stack>
+  );
+}
