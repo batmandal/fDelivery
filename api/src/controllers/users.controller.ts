ae65b16ef144ca4d78e8b00e85094dda2692818b
Bat Mandal <batmandal@Bats-MacBook-Pro.local>
2024-02-23 08:43:45 +0800

    user controller

--- api/src/controllers/users.controller.ts
+++ api/src/controllers/users.controller.ts
@@ -1,13 +1,21 @@
 import { RequestHandler } from "express";
 import { UserModel } from "../models";
 
+import jwt from "jsonwebtoken";
+
 export const getAllUsers: RequestHandler = async (req, res) => {
   const users = await UserModel.find({});
 
   res.json(users);
 };
-// export const getUser: RequestHandler = async (req, res) => {
-//   const user = await UserModel.findOne();
-
-//   res.json(user);
-// };
+export const getUser: RequestHandler = async (req, res) => {
+  const { email } = req.body;
+  // const token = jwt.sign({ email }, "secret");
+  const user = await UserModel.findOne(email);
+  if (!user) {
+    return res.status(401).json({
+      message: "not found user",
+    });
+  }
+  res.json(user);
+};
